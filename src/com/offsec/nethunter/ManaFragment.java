b13d362035a00f7485f997189ebdc4dec5791ed2
binkybear <binkybear@nethunter.com>
2016-04-06 21:19:59 -0500

    [hostapd-wpe] Fix configuration/run directly
    * Fix incorrectly named hostapd.eap_user
    * Run hostapd directly
    * Add wlan1 up
    
    https://github.com/offensive-security/nethunter-app/issues/128
    Signed-off-by: binkybear <binkybear@nethunter.com>

--- src/com/offsec/nethunter/ManaFragment.java
+++ src/com/offsec/nethunter/ManaFragment.java
@@ -171,7 +171,7 @@ public class ManaFragment extends Fragment implements ActionBar.TabListener {
                         break;
                     case 4:
                         nh.showMessage("Starting HOSTAPD-WPE");
-                        intentClickListener_NH(nh.makeTermTitle("HOSTAPD-WPE") + "/usr/bin/start-hostapd-wpe.sh");
+                        intentClickListener_NH(nh.makeTermTitle("HOSTAPD-WPE") + "ifconfig wlan1 up && /usr/bin/hostapd-wpe /sdcard/nh_files/configs/hostapd-wpe.conf");
                         break;
                     default:
                         nh.showMessage("Invalid script!");
