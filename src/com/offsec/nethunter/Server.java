8b00461b6584f154260ec64b0061a46910667ea4
binkybear <binkybear@nethunter.com>
2016-09-20 19:50:46 -0500

    Bind TCP server to localhost only
    We don’t need to broadcast our location to everyone on network…
    
    Signed-off-by: binkybear <binkybear@nethunter.com>

--- src/com/offsec/nethunter/Server.java
+++ src/com/offsec/nethunter/Server.java
@@ -6,8 +6,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketAddress;
 import java.util.ArrayList;
 
 public class Server {
@@ -30,8 +33,10 @@ public class Server {
 
     public static void main() {
         try {
-            Log.d("Server: ", "Starting server on port 9000");
-            server = new ServerSocket(9000);
+            Log.d("Server: ", "Starting server on port 9000 bound to localhost only");
+            SocketAddress socketAddress = new InetSocketAddress( "127.0.0.1", 9000);
+            server = new ServerSocket();
+            server.bind(socketAddress);
 
             while (true) {
 
