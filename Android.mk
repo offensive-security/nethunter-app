0251c3beb4b1f66097f52b4ff150444f46b55c24
binkybear <binkybear@nethunter.com>
2016-10-26 19:16:05 -0500

    Android.mk fix
    This is not the terminal emulator you are looking for…
    
    Signed-off-by: binkybear <binkybear@nethunter.com>

--- Android.mk
+++ Android.mk
@@ -2,24 +2,24 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := Nethunter-Terminal-Emulator
+LOCAL_MODULE := Nethunter
 LOCAL_MODULE_TAGS := optional
-LOCAL_PACKAGE_NAME := Nethunter-Terminal-Emulator
+LOCAL_PACKAGE_NAME := Nethunter
 
-nhterm_root  := $(LOCAL_PATH)
-nhterm_dir   := app
-nhterm_out   := $(PWD)/$(OUT_DIR)/target/common/obj/APPS/$(LOCAL_MODULE)_intermediates
-nhterm_build := $(nhterm_root)/$(nhterm_dir)/build
-nhterm_apk   := build/outputs/apk/$(nhterm_dir)-release-unsigned.apk
+nethunter_root  := $(LOCAL_PATH)
+nethunter_dir   := app
+nethunter_out   := $(PWD)/$(OUT_DIR)/target/common/obj/APPS/$(LOCAL_MODULE)_intermediates
+nethunter_build := $(nethunter_root)/$(nethunter_dir)/build
+nethunter_apk   := build/outputs/apk/$(nethunter_dir)-release-unsigned.apk
 
-$(nhterm_root)/$(nhterm_dir)/$(nhterm_apk):
-	rm -Rf $(nhterm_build)
-	mkdir -p $(nhterm_out)
-	ln -sf $(nhterm_out) $(nhterm_build)
-	cd $(nhterm_root)/$(nhterm_dir) && gradle assembleRelease
+$(nethunter_root)/$(nethunter_dir)/$(nethunter_apk):
+	rm -Rf $(nethunter_build)
+	mkdir -p $(nethunter_out)
+	ln -sf $(nethunter_out) $(nethunter_build)
+	cd $(nethunter_root)/$(nethunter_dir) && gradle assembleRelease
 
 LOCAL_CERTIFICATE := platform
-LOCAL_SRC_FILES := $(nhterm_dir)/$(nhterm_apk)
+LOCAL_SRC_FILES := $(nethunter_dir)/$(nethunter_apk)
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 
