cfd6d5a9ad4d23fbe3ba117b2037bdc0ea472a80
binkybear <binkybear@nethunter.com>
2016-10-26 19:22:42 -0500

    Tweak android.mk
    Signed-off-by: binkybear <binkybear@nethunter.com>

--- Android.mk
+++ Android.mk
@@ -7,7 +7,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_PACKAGE_NAME := Nethunter
 
 nethunter_root  := $(LOCAL_PATH)
-nethunter_dir   := app
 nethunter_out   := $(PWD)/$(OUT_DIR)/target/common/obj/APPS/$(LOCAL_MODULE)_intermediates
 nethunter_build := $(nethunter_root)/$(nethunter_dir)/build
 nethunter_apk   := build/outputs/apk/$(nethunter_dir)-release-unsigned.apk
@@ -16,10 +15,10 @@ $(nethunter_root)/$(nethunter_dir)/$(nethunter_apk):
 	rm -Rf $(nethunter_build)
 	mkdir -p $(nethunter_out)
 	ln -sf $(nethunter_out) $(nethunter_build)
-	cd $(nethunter_root)/$(nethunter_dir) && gradle assembleRelease
+	cd $(nethunter_root) && gradle assembleRelease
 
 LOCAL_CERTIFICATE := platform
-LOCAL_SRC_FILES := $(nethunter_dir)/$(nethunter_apk)
+LOCAL_SRC_FILES := $(nethunter_apk)
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 
